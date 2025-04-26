f854cee252444ab4d4e2f23d9d4e143a37dbb583
Selina Song <selinasong6@gmail.com>
2025-04-26 02:03:47 -0700

    rename hot_to_go

--- backend/src/extractor.py
+++ backend/src/extractor.py
@@ -6,7 +6,7 @@ import os
 
 mp_pose = mp.solutions.pose
 
-video_name = 'hottogo.mov'
+video_name = 'hot_to_go.mov'
 video_path = 'ground-truth-videos/' + video_name
 output_keypoints_path = 'keypoints/'
 temp_frames_path = 'temp_frames/'
